c661cb4b27594ee36f72feef9b0d6bc970d88ec1
Dan Puttick <dputtick@users.noreply.github.com>
2016-05-06 16:46:32 -0400

    added binary string diff

--- agent_based.py
+++ agent_based.py
@@ -5,23 +5,39 @@ import random as r
 
 
 class Agent():
-	def __init__(self, id):
-		self.id = id
-		self.connections = []
-		self.knowledge_state = '{0:05b}'.format(r.randint(0, 31))
+    def __init__(self, id, reality_size):
+        self.id = id
+        self.connections = []
+        self.knowledge_state = binary_string(reality_size)
 
-	def say_connections(self):
-		print(self.id, self.connections)
+    def say_connections(self):
+        print(self.id, self.connections)
 
-	def say_knowledge(self):
-		print(self.id, self.knowledge_state)
+    def say_knowledge(self):
+        print(self.id, self.knowledge_state)
 
 
-def main():
-	agents = {i: Agent(i) for i in range(6)}
-	for agent in agents:
-		agents[agent].say_knowledge()
+def binary_string(length):
+    end_range = 2**length - 1
+    string_root = '{0:0' + str(length) + 'b}'
+    return string_root.format(r.randint(0, end_range))
+
+
+def binary_string_diff(string1, string2):
+    result_string = ''
+    for index, value in enumerate(string1):
+        if value == string2[index]:
+            result_string += '1'
+        else:
+            result_string += '0'
+    return result_string, result_string.count('1')
+
+def main(reality_size=5):
+    reality = binary_string(reality_size)
+    agents = {i: Agent(i, reality_size) for i in range(6)}
+    for agent in agents:
+        print(binary_string_diff(reality, agents[agent].knowledge_state))
 
 
 if __name__ == '__main__':
-	main()
+    main()
